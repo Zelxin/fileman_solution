21dc7bf9e3ffe2f618568675959b73ad8f2d8984
vvassallo <vvassallo@savagedata.com>
2020-02-11 15:45:47 -0500

    added the csv files.

--- FileManipulation_/My Project/AssemblyInfo.vb
+++ FileManipulation_/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2020.2.11.1829")>
-<Assembly: AssemblyFileVersion("2020.2.11.1829")>
+<Assembly: AssemblyVersion("2020.2.11.2045")>
+<Assembly: AssemblyFileVersion("2020.2.11.2045")>
